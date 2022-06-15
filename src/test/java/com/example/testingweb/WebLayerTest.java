d35fe031fabf37da0644a78e34c16bde237fa1d6
Sergio Lurbe <Sergio.Lurbe@gft.com>
2022-06-15 12:45:27 +0200

    a

--- src/test/java/com/example/testingweb/WebLayerTest.java
+++ src/test/java/com/example/testingweb/WebLayerTest.java
@@ -1,5 +1,6 @@
 package com.example.testingweb;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -7,10 +8,10 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 
 @WebMvcTest
 public class WebLayerTest {
