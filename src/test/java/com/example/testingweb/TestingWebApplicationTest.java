6ae95066494ed6ca55520fe2c3eb2e23cb1aaaac
Sergio Lurbe <Sergio.Lurbe@gft.com>
2022-06-15 15:04:45 +0200

    a

--- src/test/java/com/example/testingweb/TestingWebApplicationTest.java
+++ src/test/java/com/example/testingweb/TestingWebApplicationTest.java
@@ -2,6 +2,7 @@ package com.example.testingweb;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -22,7 +23,9 @@ public class TestingWebApplicationTest {
 
     @Test
     public void shouldReturnDefaultMessage() throws Exception {
-        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+        this.mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
                 .andExpect(content().string(containsString("Hello, World")));
     }
 }
