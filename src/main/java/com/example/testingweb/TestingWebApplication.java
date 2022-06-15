55fb1b3054463c2167d6735dffbaf3ea6920e0b6
Sergio Lurbe <Sergio.Lurbe@gft.com>
2022-06-15 16:02:15 +0200

    a

--- src/main/java/com/example/testingweb/TestingWebApplication.java
+++ src/main/java/com/example/testingweb/TestingWebApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestingWebApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TestingWebApplication.class, args);
 	}
 
--- src/test/java/com/example/testingweb/WebMockTest.java
+++ src/test/java/com/example/testingweb/WebMockTest.java
@@ -15,18 +15,20 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(GreetingController.class)
+@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
 public class WebMockTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private GreetingService service;
+    //@MockBean
+    //private GreetingService service;
 
     @Test
     public void greetingShouldReturnMessageFromService() throws Exception {
         when(service.greet()).thenReturn("Hello, Mock");
-        this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
+        this.mockMvc.perform(get("/greeting"))
+                .andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string(containsString("Hello, Mock")));
     }
 }
